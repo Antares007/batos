07c2855ae602729b46a68d95c8bbcd3e66f05c28
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-05-17 08:06:11 +0400

    tword chain

--- s.c
+++ s.c
@@ -9,81 +9,83 @@
 #ifndef ChapterSize
 #define ChapterSize 512
 #endif
-#define CS ChapterSize
 
 #define P (void)0 // printf("%s\n", __func__)
 // #define P printf("%2ld %10s %c\n", t, __func__, (char)o[t+1])
 
 #include <stdio.h>
-
-#define N(argo) void argo(void*b, long *o, long t, long *Blue)
+#define Context long *o, long t, struct Tword *tword, long s
+#define N(argo) long argo(Context)
+#define C o, t, tword, s
+typedef struct Tword {
+  struct Tword*upper;
+  long t;
+  N((*right));
+  N((*clr));
+} Tword;
 typedef N((*n_t));
-
-// Chapter N(Green_) { P, ((n_t)Blue[5])(b, o, t, Blue); }
-// Chapter N(Green_D) { P, ((n_t)Blue[5])(b, o, t, Blue); }
-// Chapter N(Green_B) { P; printf("2%c", (char)o[t + 1]), (Green_ + o[t + 2] *
-// CS)(b, o, t + 2, Blue); } Chapter N(Green_T) { P; long Blue2[] = {Blue, 0, 0,
-// 0, o[t + 1], cont, t}; (Blue_ + o[0] * CS)(b, o, 0, Blue2); }
-//
-static N(bo) {(((n_t)b) + o[t] * ChapterSize)(b, o, t, Blue);}
-
-N(Blue_);
-N(Yellow_);
-N(cont) {
-  P;
-  t = Blue[6];
-  Blue = Blue[0];
-  (Yellow_ + o[t + 2] * CS)(b, o, t + 2, Blue);
+static inline long book_of(n_t book, Context) {
+  return (book + o[t] * ChapterSize)(C);
 }
-Chapter N(Yellow_) { P, ((n_t)Blue[5])(b, o, t, Blue); }
-Chapter N(Yellow_D) { P, ((n_t)Blue[5])(b, o, t, Blue); }
-Chapter N(Yellow_B) { P; printf("%c", (char)o[t+1]), bo(Yellow_, o, t + 2, Blue); }
-Chapter N(Yellow_T) { P, bo(Blue_, o, 0, (long[]){Blue, 0, 0, 0, o[t + 1], cont, t}); }
-Chapter N(Blue_   ) { P;  }
-Chapter N(Blue_D  ) { P;
-                      if (Blue[4] == o[t + 1])
-                        bo(Yellow_, o, t + 2, Blue);
-                      bo(b, o, t + 2, Blue); }
-Chapter N(Blue_B  ) { bo(b, o, t + 2, Blue); }
-Chapter N(Blue_T  ) { bo(b, o, t + 2, Blue); }
 
-N(capstone) { P; }
+N(Blue_0);
+N(Green_0);
+
+// int clr_top(Context) { return b[3] == o[t]; }
+// int clr(Context) {
+//  //if (b[3] == o[t]) {
+//  //} else {
+//  //  long ut = ((n_t)b[5])(o,t,b[0]);
+//  //  return t;
+//  //}
+//}
+
+Chapter N(Green_0 ) { return tword->right(C); }
+Chapter N(Green_1 ) { return tword->right(C); }
+Chapter N(Green_2 ) { return printf("%c", (char)o[t + 1]), book_of(Green_0, o, t + 2, tword, s); }
+        N(cont    ) { return book_of(Green_0, o, tword->t + 2, tword->upper, s); }
+Chapter N(Green_3 ) { return book_of(Blue_0, o, 0, &(Tword){.upper = tword, .t = t, .right = cont, .clr = 0}, s); }
+
+Chapter N(Yellow_0) { return tword->right(C); }
+Chapter N(Yellow_1) { return tword->right(C); }
+Chapter N(Yellow_2) { return printf("%c", (char)o[t + 1]), book_of(Green_0, o, t + 2, tword, s); }
+Chapter N(Yellow_3) { return book_of(Blue_0, o, 0, &(Tword){.upper = tword, .t = t, .right = cont, .clr = 0}, s); }
+
+Chapter N(Blue_0  ) { o[s+t] = o[t] + 4, o[s+t+1] = o[t+1] + 4; return s + t; }
+Chapter N(Blue_1  ) { if (o[tword->t + 1] == o[t + 1])
+                        book_of(Yellow_0, o, t + 2, tword, s);
+                      book_of(Blue_0, o, t + 2, tword, s);
+                      return t;
+                    }
+Chapter N(Blue_2  ) { return book_of(Blue_0, o, t + 2, tword, s); }
+Chapter N(Blue_3  ) { return book_of(Blue_0, o, t + 2, tword, s); }
+
 
 static long ram[2048];
-N(ret) {
-  P;
-//  char buff[1024];
-//  int len = 0;
-//  while(b)
-//    buff[len++] = *b, b = b[1];
-//  while(len--) 
-//    printf("%c", buff[len]);
-  printf("\n");
-}
+N(ret) { return printf("\n"), t; }
 int main() {
   long *o = ram + 1024;
-  long b = 0;
-  long t = 0;
   long s = 0;
 
 #define D(S) o[s++] = 1, o[s++] = S
 #define B(b) o[s++] = 2, o[s++] = b
 #define T(S) o[s++] = 3, o[s++] = S
-
-  D('S'), B('b');         // S -> 'b'
-  D('S'), T('S'), B('a'); // S -> S 'a'
-                          //
-  D('A'), B('a');         // A -> 'a'
-  D('A'), T('B'), B('a'); // A -> B 'a'
-  D('B'), B('b');         // B -> 'b'
-  D('B'), T('O'), B('b'); // B -> A 'b'
-                          //
+  D('.'), T('3');                 // . -> 3 - argumented grammar rule
+                                  //
+  D('S'), B('b');                 // S -> 'b'
+  D('S'), T('S'), B('a');         // S -> S 'a'
+                                  //
+  D('A'), B('a');                 // A -> 'a'
+  D('A'), T('B'), B('a');         // A -> B 'a'
+  D('B'), B('b');                 // B -> 'b'
+  D('B'), T('A'), B('b');         // B -> A 'b'
+                                  //
   D('3'), T('T'), T('T'), T('T'); // 3 -> T T T '\n'
-  D('T'), B('t');         // T -> 't'
-  D('T'), B('a');         // T -> 'a'
-  D('T'), B('b');         // T -> 'b'
+  D('T'), B('t');                 // T -> 't'
+  D('T'), B('a');                 // T -> 'a'
+  D('T'), B('b');                 // T -> 'b'
 
   o[s++] = 0;
-  long cap[] = {0, 0, 0, 0, capstone};
-  bo(Blue_, o, t, (long[]){cap, cap, cap, cap, '3', ret});
+  o[s++] = 0;
+  book_of(Blue_0, o, 0, &(Tword){.upper = 0, .t = 0, .right = ret, .clr = 0}, s);
 }
