de0502972a47f604b15d82eb375e460eb5ef4fe0
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-06-01 08:34:03 +0400

    nice working demo

--- s.c
+++ s.c
@@ -1,140 +1,208 @@
-// clang-format off
 #pragma GCC diagnostic ignored "-Wint-conversion"
 
-#define _STR(s) #s
-#define STR(s) _STR(s)
-
-#define Chapter                                                                \
-  __attribute__((section(".text." __FILE_NAME__ STR(__LINE__))))               \
-  __attribute__((__noinline__)) __attribute__((used))
-#define ChapterSize 512
-
-#include <stdio.h>
-#define Context void*b, long *o, long t, struct Quattro *q, long s, long*u
+#define Context long b, long *o, long t, struct Ray *u, long s, long d
 #define N(argo) long argo(Context)
-#define C b, o, t, q, s
+#define C b, o, t, u, s
+
 typedef struct Ray {
-  struct Quattro*upper;
+  struct Ray *upper;
   long t;
-  N((*partition));
-  N((*new_partition));
-  N((*continue_or_return_to_next));
-  N((*clr));
+  long partition;
 } Ray;
-typedef struct Quattro {
-  Ray*Blue;
-  Ray*Green;
-  Ray*Yellow;
-  Ray*Red;
-} Quattro;
+
 typedef N((*n_t));
+typedef enum { Blue, Green, Yellow, Red, Pink } Rays;
+static const char *ray_names[] = {"Blue", "Green", "Yellow", "Red"};
+
 #define S(argo) static N(argo)
 
-S(Blue_0); S(Green_0); S(Yellow_0); S(Red_0);
-S(bo) { return (((n_t)b) + o[t] * ChapterSize)(C, u); }
+S(reTurn) { return t; }
 
-Chapter S(Blue_0  ) { return t; }
-Chapter S(Blue_1  ) { Quattro*p = q; int depth = 0; while(p) p = p->Blue->upper, depth++;
-                      if (7 < depth) return t;
-                      if (o[q->Blue->t + 1] == o[t + 1]) bo(q->Blue->new_partition, o, t + 2, q, s, u);
-                      return bo(b, o, t + 2, q, s, u); }
-Chapter S(Blue_2  ) { return bo(b, o, t + 2, q, s, u); }
-Chapter S(Blue_3  ) { return bo(b, o, t + 2, q, s, u); }
+static N(book_of_text);
+S(next_text_member) { return book_of_text(b, o, t + 2, u, s, d); }
 
-S(clr_top         ) { return o[q->Blue->t + 1] == o[t + 1]; }
-S(clr             ) { return o[q->Blue->t + 1] == o[t + 1] ? 2 : q->Blue->upper->Blue->clr(b, o, t, q->Blue->upper, s, u); }
-S(cont            ) { return bo(b, o, q->Blue->t + 2, q->Blue->upper, s, u); }
-S(upper_cont      ) { return q->Blue->upper->Blue->continue_or_return_to_next(b, o, t, q->Blue->upper, s, u); }
+S(continue_toti) { return next_text_member(b, o, u->t, u->upper, s, d); }
+S(continue_toti_as_Blue) { return continue_toti(Blue, o, t, u, s, d); }
+S(continue_toti_as_Green) { return continue_toti(Green, o, t, u, s, d); }
 
-Chapter S(Green_0 ) { q->Blue->continue_or_return_to_next(C, u);
-                      Ray r = { .upper = q,
-                                .t = q->Blue->t,
-                                .partition = b,
-                                .new_partition = Red_0,
-                                .continue_or_return_to_next = upper_cont,
-                                .clr = clr_top};
-                      return bo(Blue_0, o, 0, &(Quattro){&r, q->Green, q->Yellow, q->Red}, s, u); }
-Chapter S(Green_1 ) { return Green_0(C, u); }
-Chapter S(Green_2 ) { return ((n_t)o[t + 1])(C, u); }
-Chapter S(Green_3 ) { Ray r = { .upper = q,
-                                .t = t,
-                                .partition = b,
-                                .new_partition = Yellow_0,
-                                .continue_or_return_to_next = cont,
-                                .clr = clr_top};
-                      return bo(Blue_0, o, 0, &(Quattro){&r, q->Green, q->Yellow, q->Red}, s, u); }
+S(branch_with_Red_pith);
+S(continue_toti_orand_branch_with_Red_pith) {
+  return continue_toti(C, d), branch_with_Red_pith(C, d);
+}
+S(continue_toti_as_Green_orand_branch_with_Red_pith) {
+  return continue_toti_as_Green(C, d), branch_with_Red_pith(C, d);
+}
+S(continue_toti_as_Blue_orand_branch_with_Red_pith) {
+  return continue_toti_as_Blue(C, d), branch_with_Red_pith(C, d);
+}
+S(book_of_continuation) {
+  static n_t nars[3 * 5] = {
+      continue_toti_orand_branch_with_Red_pith,          // Blue_Blue,
+      continue_toti_as_Green_orand_branch_with_Red_pith, // Blue_Green,
+      continue_toti_as_Green_orand_branch_with_Red_pith, // Blue_Yellow,
+      continue_toti,                                     // Blue_Red,
+      branch_with_Red_pith,                              // Blue_Pink,
+                                                         //
+      continue_toti_as_Blue_orand_branch_with_Red_pith,  // Green_Blue,
+      continue_toti_orand_branch_with_Red_pith,          // Green_Green,
+      continue_toti_orand_branch_with_Red_pith,          // Green_Yellow,
+      0,                                                 // Green_Red,
+      branch_with_Red_pith,                              // Green_Pink
+                                                         //
+      continue_toti_as_Blue,                             // Yellow_Blue
+      continue_toti_as_Green,                            // Yellow_Green
+      continue_toti,                                     // Yellow_Yellow
+      0,                                                 // Yellow_Red
+      reTurn,                                            // Yellow_Pink
+  };
+  return nars[b * 5 + u->partition](C, d);
+}
 
-Chapter S(Yellow_0) { return Green_0(q->Blue->partition, o, t, q, s, u); }
-Chapter S(Yellow_1) { return Yellow_0(C, u); }
-Chapter S(Yellow_2) { return Green_2(C, u); }
-Chapter S(Yellow_3) { if (q->Blue->clr(C, u)) return 0;
-                      Ray r = { .upper = q,
-                                .t = t,
-                                .partition = b,
-                                .new_partition = Yellow_0,
-                                .continue_or_return_to_next = cont,
-                                .clr = clr};
-                      return bo(Blue_0, o, 0, &(Quattro){&r, q->Green, q->Yellow, q->Red}, s, u); }
+static N(book_of_branching);
+N(grow_toti_with_Yellow_pith) {
+  return book_of_branching(Yellow, o, 0,
+                           &(Ray){.upper = u, .t = t, .partition = b}, s, d);
+}
 
-Chapter S(Red_0   ) { return 0; }
-Chapter S(Red_1   ) { return 0; }
-Chapter S(Red_2   ) { return 0; }
-Chapter S(Red_3   ) { int rez = q->Blue->clr(C, u);
-                      if (rez == 2) return 0;
-                      if (rez == 1) return bo(Green_0, o, t + 2, q, s, u);
-                      Ray r = { .upper = q,
-                                .t = t,
-                                .partition = q->Blue->upper->Blue->partition,
-                                .new_partition = Red_0,
-                                .continue_or_return_to_next = cont,
-                                .clr = clr};
-                      return bo(Blue_0, o, 0, &(Quattro){&r, q->Green, q->Yellow, q->Red}, s, u); }
+S(branch_with_Red_pith) { return book_of_branching(Red, o, 0, u, s, d); }
+S(grow_toti_with_Red_pith) {
+  return branch_with_Red_pith(b, o, t,
+                              &(Ray){.upper = u, .t = t, .partition = b}, s, d);
+}
+S(return_or_grow_toti_with_Yellow_pith) {
+  return o[u->t + 1] == o[t + 1]
+             ? 1
+             : grow_toti_with_Yellow_pith(b, o, t, o[s], s, d);
+}
+static N(book_of_recursion);
+S(book_of_recursion_for_upper) {
+  return book_of_recursion(b, o, t, u->upper, s, d);
+}
+S(return_or_book_of_recursion_for_upper) {
+  return o[u->t + 1] == o[t + 1] ? 1 : book_of_recursion_for_upper(C, d);
+}
+S(next_text_member_as_Blue) { return next_text_member(Blue, o, t, u, s, d); }
+S(next_text_member_as_Blue_or_grow_toti_with_Red_pith) {
+  return (o[u->t + 1] == o[t + 1]
+              ? next_text_member_as_Blue
+              : grow_toti_with_Red_pith)(b, o, t, o[s], s, d);
+}
+S(book_of_recursion) {
+  static n_t nars[4 * 5] = {
+      grow_toti_with_Yellow_pith,                          // Blue_Blue,
+      grow_toti_with_Yellow_pith,                          // Blue_Green,
+      grow_toti_with_Yellow_pith,                          // Blue_Yellow,
+      grow_toti_with_Yellow_pith,                          // Blue_Red,
+      grow_toti_with_Yellow_pith,                          // Blue_Pink,
+                                                           //
+      grow_toti_with_Yellow_pith,                          // Green_Blue,
+      grow_toti_with_Yellow_pith,                          // Green_Green,
+      grow_toti_with_Yellow_pith,                          // Green_Yellow,
+      0,                                                   // Green_Red,
+      grow_toti_with_Yellow_pith,                          // Green_Pink,
+                                                           //
+      return_or_grow_toti_with_Yellow_pith,                // Yellow_Blue
+      return_or_grow_toti_with_Yellow_pith,                // Yellow_Green
+      return_or_book_of_recursion_for_upper,               // Yellow_Yellow
+      0,                                                   // Yellow_Red
+      return_or_grow_toti_with_Yellow_pith,                // Yellow_Pink
+                                                           //
+      next_text_member_as_Blue_or_grow_toti_with_Red_pith, // Red_Blue
+      next_text_member_as_Blue_or_grow_toti_with_Red_pith, // Red_Green
+      next_text_member_as_Blue_or_grow_toti_with_Red_pith, // Red_Yellow
+      return_or_book_of_recursion_for_upper,               // Red_Red
+      next_text_member_as_Blue_or_grow_toti_with_Red_pith, // Red_Pink
+  };
+  return nars[b * 5 + u->partition](C, d);
+}
+static N(book_of_branching);
+S(skip_next_text_member) { return book_of_branching(b, o, t + 2, u, s, d); }
+S(if_def_next_text_member_orand_find_next_def) {
+  if (o[u->t + 1] == o[t + 1])
+    next_text_member(C, d);
+  return skip_next_text_member(C, d);
+}
+S(book_of_branching) {
+  static n_t nars[4] = {
+      reTurn,                                      // 0
+      if_def_next_text_member_orand_find_next_def, // D(1)efintion
+      skip_next_text_member,                       // B(2)eginning
+      skip_next_text_member,                       // T(3)oti
+  };
+  return nars[o[t]](C, d);
+}
+S(ok) { return ((n_t)o[s])(C, d); }
+S(os_book_of_recursion) { return o[s] = u, book_of_recursion(C, d); }
+S(dispatch_next_text_member) {
+  return o[s] = next_text_member, ((n_t)o[t + 1])(C, d);
+}
+S(next_text_member_as_Green) { return next_text_member(Green, o, t, u, s, d); }
+S(dispatch_next_text_member_as_Green) {
+  return o[s] = next_text_member_as_Green, ((n_t)o[t + 1])(C, d);
+}
+S(book_of_text) {
+  static n_t nars[4 * 4] = {
+      book_of_continuation,               // Blue
+      book_of_continuation,               // Blue_1
+      dispatch_next_text_member,          // Blue_2
+      os_book_of_recursion,               // Blue_3
+                                          //
+      book_of_continuation,               // Green
+      book_of_continuation,               // Green_1
+      dispatch_next_text_member,          // Green_2
+      os_book_of_recursion,               // Green_3
+                                          //
+      book_of_continuation,               // Yellow
+      book_of_continuation,               // Yellow_1
+      dispatch_next_text_member_as_Green, // Yellow_2
+      os_book_of_recursion,               // Yellow_3
+                                          //
+      reTurn,                             // Red
+      reTurn,                             // Red_1
+      reTurn,                             // Red_2
+      os_book_of_recursion,               // Red_3
+  };
+  return nars[b * 4 + o[t]](C, d);
+}
+static char str[1024];
+extern int printf(const char *restrict __format, ...);
+#define Length 4
+// clang-foramt off
+S(term_t) { return (Length == d ? 0 : (str[d] = 't', ok(C, d + 1))); }
+S(term_a) { return (Length == d ? 0 : (str[d] = 'a', ok(C, d + 1))); }
+S(term_b) { return (Length == d ? 0 : (str[d] = 'b', ok(C, d + 1))); }
+S(term_o) { return (Length == d ? 0 : (str[d] = 'o', ok(C, d + 1))); }
+S(term_s) { return (Length == d ? 0 : (str[d] = 's', ok(C, d + 1))); }
+S(term_1) { return (Length == d ? 0 : (str[d] = '1', ok(C, d + 1))); }
+S(term_2) { return (Length == d ? 0 : (str[d] = '2', ok(C, d + 1))); }
+S(term_3) { return (Length == d ? 0 : (str[d] = '3', ok(C, d + 1))); }
+// clang-foramt on
 
+S(term_nl) {
+  str[d] = '\0';
+  printf("%s\n", str);
+  return ok(C, d);
+}
 
-static long ram[2048];
-S(ret) { return 0; }
-#include <assert.h>
-S(ok      ) { return bo(Green_0, o, t+2, q, s, u); }
-
-S(term_t  ) { return ok(C, (long[]){"t", u}); }
-S(term_a  ) { return ok(C, (long[]){"a", u}); }
-S(term_b  ) { return ok(C, (long[]){"b", u}); }
-S(term_o  ) { return ok(C, (long[]){"o", u}); }
-S(term_1  ) { return ok(C, (long[]){"1", u}); }
-S(term_2  ) { return ok(C, (long[]){"2", u}); }
-S(term_3  ) { return ok(C, (long[]){"3", u}); }
-S(term_nl ) {
-  const char*strs[100];
-  int len = 0;
-  void**l = (void*)u;
-  while(l) strs[len++] = l[0], l = l[1];
-  while(len) printf("%s", strs[--len]);
-  printf("\n");
-  return bo(Green_0, o,t+2,q,s, u); }
+// book oriented programming
+// book operator?
 int main() {
-  long *o = ram + 1024;
+  long o[2048];
   long s = 0;
-  long t = s;
-  long u = 0;
-
-  // partition-supported programming through linker script
-  // partition operator?
-  assert((Red_0 - Blue_0) / ChapterSize / 4 == 3);
-
 #define D(S) o[s++] = 1, o[s++] = S
 #define B(b) o[s++] = 2, o[s++] = term_##b
 #define T(S) o[s++] = 3, o[s++] = S
 
   D('_'), T('3'), B(nl);
-  D('3'), T('T'), T('T'), T('T');
+  D('_'), T('S'), B(nl);
+  D('_'), T('A'), B(nl);
   D('T'), B(t);
-  D('T'), B(a);
-  D('T'), B(b), T('N');
   D('N'), B(1);
+  D('T'), B(a);
+  D('3'), T('T'), T('T'), T('T');
   D('N'), B(2);
   D('N'), B(3);
-
-  D('s'), T('s');
+  D('T'), B(b);
 
   D('S'), B(b);
   D('S'), T('S'), B(a);
@@ -145,23 +213,16 @@ int main() {
   D('A'), B(b);
   D('A'), T('B'), B(a);
   D('B'), B(t);
-  D('B'), T('A'), B(o);
+  D('B'), T('A'), B(s);
 
   o[s++] = 0;
   o[s++] = 0;
 
-  Ray r = {
-    .upper = 0,
-    .t = 0,
-    .partition = Yellow_0,
-    .new_partition = Yellow_0,
-    .continue_or_return_to_next = ret,
-    .clr = clr_top
-  };
-  bo(Blue_0, o, 0, &(Quattro){&r, 0, 0, 0}, s, u);
+  grow_toti_with_Yellow_pith(Pink, o, 0, 0, s, 0);
   return 0;
 }
 /*
+
 primary_expression:
   constant
   (expression)
