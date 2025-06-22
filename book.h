6b79efe810a87c286a52fa729da4eeb0aaa51bf5
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-06-22 08:40:38 +0400

    remove extra

--- book.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define _STR(s) #s
-#define STR(s) _STR(s)
-
-#define Chapter                                                                \
-  __attribute__((section(".text." __FILE_NAME__ STR(__LINE__))))               \
-  __attribute__((__noinline__)) __attribute__((used))
